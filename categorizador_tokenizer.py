fcf1bf24a1fbb0204ae4210fe7261732efe66b7e
Vick Durazzo <marividurazzo007@gmail.com>
2024-02-07 08:22:08 -0300

    Create categorizador_tokenizer.py
    Script que escolhe o modelo da IA de forma dinâmica

---
+++ categorizador_tokenizer.py
@@ -0,0 +1,57 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import tiktoken
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-3.5-turbo"
+
+codificador = tiktoken.encoding_for_model(modelo)
+
+def carrega(nome_do_arquivo):
+    try:
+        with open(nome_do_arquivo, "r") as arquivo:
+            dados = arquivo.read()
+            return dados
+    except IOError as e:
+        print(f"Erro: {e}")
+
+prompt_sistema = """
+Identifique o perfil de compra para cada cliente a seguir.
+
+O formato de saída deve ser:
+
+cliente - descreva o perfil do cliente em 3 palavras
+"""
+
+prompt_usuario = carrega("dados\lista_de_compras_100_clientes.csv")
+
+arquivo_csv = "./dados/lista_de_compras_100_clientes.csv"
+dados_usuario = carrega(arquivo_csv)
+lista_tokens = codificador.encode(dados_usuario)
+numero_tokens = len(lista_tokens)
+#se o tamanho for maior do que o esperado e maior do que o suportado pelo modelo
+modelo = "GPT-3.5-turbo" if numero_tokens < 4096 else "GPT-4"
+
+
+print(f"Modelo escolhido: {modelo}")
+
+lista_mensagens = [
+        {
+            "role": "system",
+            "content": prompt_sistema
+        },
+        {
+            "role": "user",
+            "content": prompt_usuario
+        }
+    ]
+
+resposta = client.chat.completions.create(
+    messages = lista_mensagens,
+    model=modelo
+)
+
+print(resposta.choices[0].message.content)
